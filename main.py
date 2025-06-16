52ecd58bfb2c085e9fe0d9cc1eec5b3c8f9adcc9
Freddy1e100 <freddy1e100@gmail.com>
2025-06-17 00:59:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import mplfinance as mpf
 from binance.client import Client
 from ta.momentum import StochRSIIndicator
 import datetime
